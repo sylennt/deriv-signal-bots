ccca22118b57b52f6b52677cd73644eec263e02b
sylennt <160680535+sylennt@users.noreply.github.com>
2026-02-14 22:46:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ def run():
         "entry": 100,
         "stop_loss": 95,
         "take_profit": 110,
-        "reason": "Forced test from main.py"
+        "reason": "Forced test from GitHub"
     }
 
-    print("About to send Telegram alert")
     send_telegram_alert(test_trade)
-    print("Telegram function executed")
+
+if __name__ == "__main__":
+    run()
