cac6a65295dd81c42f5fd5f09334007b990c1849
sylennt <160680535+sylennt@users.noreply.github.com>
2026-02-14 23:56:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+from strategy import analyze
 from telegram_alert import send_telegram_alert
 
-def run():
-    test_trade = {
-        "signal": "BUY",
-        "entry": 100,
-        "stop_loss": 95,
-        "take_profit": 110,
-        "reason": "Forced test from GitHub"
-    }
+SYMBOLS = ["R_100", "R_75", "R_50"]
 
-    send_telegram_alert(test_trade)
+def run():
+    for symbol in SYMBOLS:
+        trade = analyze(symbol)
+
+        if trade and trade["signal"] != "NO_TRADE":
+            send_telegram_alert(trade)
 
 if __name__ == "__main__":
     run()
